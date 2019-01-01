66db8cd4c937157993ae244b31fec53409c58128
Sacha Servan-Schreiber <sachaservanschreiber@gmail.com>
2019-01-01 18:55:09 -0500

    fix spacing

--- benchmark/benchmark_bible.sh
+++ benchmark/benchmark_bible.sh
@@ -4,15 +4,15 @@
 
 # java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/bible.txt" -s 0.40 -z 10000 -d 5455350 -r "/usr/local/bin/shuf" -i "n_ps-bible-lg-0.40-10k" -o "./results/n_ps-bible-lg-0.40-10k_results_runtime_correctness.json" -n 5 --benchmarkRuntime --prosecco
 # java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/bible.txt" -s 0.40 -z 10000 -d 5455350 -r "/usr/local/bin/shuf" -i "n_ps-bible-lg-0.40-10k" -o "./results/n_ps-bible-lg-0.40-10k_results_memory.json" -n 5 --benchmarkMemory --prosecco
-java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/bible.txt" -s 0.40 -z 10000 -d 5455350 -r "/usr/local/bin/shuf" -i "n_ps-bible-lg-0.40-10k_prefixspan" -o "./results/n_ps-bible-lg-0.40-10k_results_prefixspan.json"-n 5 --benchmarkRuntime --prefixspan
+java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/bible.txt" -s 0.40 -z 10000 -d 5455350 -r "/usr/local/bin/shuf" -i "n_ps-bible-lg-0.40-10k_prefixspan" -o "./results/n_ps-bible-lg-0.40-10k_results_prefixspan.json" -n 5 --benchmarkRuntime --prefixspan
 
 # java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/bible.txt" -s 0.50 -z 10000 -d 5455350 -r "/usr/local/bin/shuf" -i "n_ps-bible-lg-0.50-10k" -o "./results/n_ps-bible-lg-0.50.3-10k_results_runtime_correctness.json" -n 5 --benchmarkRuntime --prosecco
 # java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/bible.txt" -s 0.50 -z 10000 -d 5455350 -r "/usr/local/bin/shuf" -i "n_ps-bible-lg-0.50-10k" -o "./results/n_ps-bible-lg-0.50-10k_results_memory.json" -n 5 --benchmarkMemory --prosecco
-java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/bible.txt" -s 0.50 -z 10000 -d 5455350 -r "/usr/local/bin/shuf" -i "n_ps-bible-lg-0.50-10k_prefixspan" -o "./results/n_ps-bible-lg-0.50-10k_results_prefixspan.json"-n 5 --benchmarkRuntime --prefixspan
+java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/bible.txt" -s 0.50 -z 10000 -d 5455350 -r "/usr/local/bin/shuf" -i "n_ps-bible-lg-0.50-10k_prefixspan" -o "./results/n_ps-bible-lg-0.50-10k_results_prefixspan.json" -n 5 --benchmarkRuntime --prefixspan
 
 # java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/bible.txt" -s 0.60 -z 10000 -d 5455350 -r "/usr/local/bin/shuf" -i "n_ps-bible-lg-0.60-10k" -o "./results/n_ps-bible-lg-0.60-10k_results_runtime_correctness.json" -n 5 --benchmarkRuntime --prosecco
 # java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/bible.txt" -s 0.60 -z 10000 -d 5455350 -r "/usr/local/bin/shuf" -i "n_ps-bible-lg-0.60-10k" -o "./results/n_ps-bible-lg-0.60-10k_results_memory.json" -n 5 --benchmarkMemory --prosecco
-java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/bible.txt" -s 0.60 -z 10000 -d 5455350 -r "/usr/local/bin/shuf" -i "n_ps-bible-lg-0.60-10k_prefixspan" -o "./results/n_ps-bible-lg-0.60-10k_results_prefixspan.json"-n 5 --benchmarkRuntime --prefixspan
+java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/bible.txt" -s 0.60 -z 10000 -d 5455350 -r "/usr/local/bin/shuf" -i "n_ps-bible-lg-0.60-10k_prefixspan" -o "./results/n_ps-bible-lg-0.60-10k_results_prefixspan.json" -n 5 --benchmarkRuntime --prefixspan
 
 
 # different block sizes
