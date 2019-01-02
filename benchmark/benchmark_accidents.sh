572ae2c3f6878808b12e323142c3a46f8d45b6c9
Sacha Servan-Schreiber <sachaservanschreiber@gmail.com>
2019-01-02 14:47:11 -0500

    uncomment scripts for last round of experiments

--- benchmark/benchmark_accidents.sh
+++ benchmark/benchmark_accidents.sh
@@ -2,17 +2,17 @@
 
 # TODO: might need to have full path!
 
-java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/accidents.txt" -s 0.80 -z 10000 -d 3401830 -r "/usr/local/bin/shuf" -i "n_ps-accidents-lg-0.80-10k" -o "./results/n_ps-accidents-lg-0.80-10k_results_runtime_correctness.json" -n 5 --benchmarkRuntime --prosecco
+#java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/accidents.txt" -s 0.80 -z 10000 -d 3401830 -r "/usr/local/bin/shuf" -i "n_ps-accidents-lg-0.80-10k" -o "./results/n_ps-accidents-lg-0.80-10k_results_runtime_correctness.json" -n 5 --benchmarkRuntime --prosecco
 # java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/accidents.txt" -s 0.80 -z 10000 -d 3401830 -r "/usr/local/bin/shuf" -i "n_ps-accidents-lg-0.80-10k" -o "./results/n_ps-accidents-lg-0.80-10k_results_memory.json" -n 5 --benchmarkMemory --prosecco
-# java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/accidents.txt" -s 0.80 -z 10000 -d 3401830 -r "/usr/local/bin/shuf" -i "n_ps-accidents-lg-0.80-10k_prefixspan" -o "./results/n_ps-accidents-lg-0.80-10k_results_prefixspan.json" -n 5 --benchmarkRuntime --prefixspan
+java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/accidents.txt" -s 0.80 -z 10000 -d 3401830 -r "/usr/local/bin/shuf" -i "n_ps-accidents-lg-0.80-10k_prefixspan" -o "./results/n_ps-accidents-lg-0.80-10k_results_prefixspan.json" -n 5 --benchmarkRuntime --prefixspan
 
-java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/accidents.txt" -s 0.85 -z 10000 -d 3401830 -r "/usr/local/bin/shuf" -i "n_ps-accidents-lg-0.85-10k" -o "./results/n_ps-accidents-lg-0.85-10k_results_runtime_correctness.json" -n 5 --benchmarkRuntime --prosecco
+# java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/accidents.txt" -s 0.85 -z 10000 -d 3401830 -r "/usr/local/bin/shuf" -i "n_ps-accidents-lg-0.85-10k" -o "./results/n_ps-accidents-lg-0.85-10k_results_runtime_correctness.json" -n 5 --benchmarkRuntime --prosecco
 # java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/accidents.txt" -s 0.85 -z 10000 -d 3401830 -r "/usr/local/bin/shuf" -i "n_ps-accidents-lg-0.85-10k" -o "./results/n_ps-accidents-lg-0.85-10k_results_memory.json" -n 5 --benchmarkMemory --prosecco
-# java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/accidents.txt" -s 0.85 -z 10000 -d 3401830 -r "/usr/local/bin/shuf" -i "n_ps-accidents-lg-0.85-10k_prefixspan" -o "./results/n_ps-accidents-lg-0.85-10k_results_prefixspan.json" -n 5 --benchmarkRuntime --prefixspan
+java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/accidents.txt" -s 0.85 -z 10000 -d 3401830 -r "/usr/local/bin/shuf" -i "n_ps-accidents-lg-0.85-10k_prefixspan" -o "./results/n_ps-accidents-lg-0.85-10k_results_prefixspan.json" -n 5 --benchmarkRuntime --prefixspan
 
-java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/accidents.txt" -s 0.90 -z 10000 -d 3401830 -r "/usr/local/bin/shuf" -i "n_ps-accidents-lg-0.90-10k" -o "./results/n_ps-accidents-lg-0.90-10k_results_runtime_correctness.json" -n 5 --benchmarkRuntime --prosecco
+#java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/accidents.txt" -s 0.90 -z 10000 -d 3401830 -r "/usr/local/bin/shuf" -i "n_ps-accidents-lg-0.90-10k" -o "./results/n_ps-accidents-lg-0.90-10k_results_runtime_correctness.json" -n 5 --benchmarkRuntime --prosecco
 # java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/accidents.txt" -s 0.90 -z 10000 -d 3401830 -r "/usr/local/bin/shuf" -i "n_ps-accidents-lg-0.90-10k" -o "./results/n_ps-accidents-lg-0.90-10k_results_memory.json" -n 5 --benchmarkMemory --prosecco
-# java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/accidents.txt" -s 0.90 -z 10000 -d 3401830 -r "/usr/local/bin/shuf" -i "n_ps-accidents-lg-0.90-10k_prefixspan" -o "./results/n_ps-accidents-lg-0.90-10k_results_prefixspan.json" -n 5 --benchmarkRuntime --prefixspan
+java -jar -Xmx40G ProseccoBenchmark.jar -f "/home/ez/data/sequence/accidents.txt" -s 0.90 -z 10000 -d 3401830 -r "/usr/local/bin/shuf" -i "n_ps-accidents-lg-0.90-10k_prefixspan" -o "./results/n_ps-accidents-lg-0.90-10k_results_prefixspan.json" -n 5 --benchmarkRuntime --prefixspan
 
 
 # different block sizes
