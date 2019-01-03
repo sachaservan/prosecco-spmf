9ed5bde2fc5447eec991139ec125cc271d05d80d
Emanuel Zgraggen <emanuel.zgraggen@gmail.com>
2019-01-03 09:38:31 -0500

    updated all_spam

--- benchmark/all_spam.sh
+++ benchmark/all_spam.sh
@@ -2,10 +2,6 @@ java -jar -Xmx120G ProseccoBenchmark.jar -f "/home/ez/data/sequence/accidents.tx
 java -jar -Xmx120G ProseccoBenchmark.jar -f "/home/ez/data/sequence/accidents.txt" -s 0.85 -z 10000 -d 3401830 -r "/usr/local/bin/shuf" -i "n_ps-accidents-lg-0.85-10k_spam" -o "./results/n_ps-accidents-lg-0.85-10k_results_spam.json" -n 5 --benchmarkRuntime --spam
 java -jar -Xmx120G ProseccoBenchmark.jar -f "/home/ez/data/sequence/accidents.txt" -s 0.90 -z 10000 -d 3401830 -r "/usr/local/bin/shuf" -i "n_ps-accidents-lg-0.90-10k_spam" -o "./results/n_ps-accidents-lg-0.90-10k_results_spam.json" -n 5 --benchmarkRuntime --spam
 
-java -jar -Xmx120G ProseccoBenchmark.jar -f "/home/ez/data/sequence/bible.txt" -s 0.40 -z 10000 -d 5455350 -r "/usr/local/bin/shuf" -i "n_ps-bible-lg-0.40-10k_spam" -o "./results/n_ps-bible-lg-0.40-10k_results_spam.json" -n 5 --benchmarkRuntime --spam
-java -jar -Xmx120G ProseccoBenchmark.jar -f "/home/ez/data/sequence/bible.txt" -s 0.50 -z 10000 -d 5455350 -r "/usr/local/bin/shuf" -i "n_ps-bible-lg-0.50-10k_spam" -o "./results/n_ps-bible-lg-0.50-10k_results_spam.json" -n 5 --benchmarkRuntime --spam
-java -jar -Xmx120G ProseccoBenchmark.jar -f "/home/ez/data/sequence/bible.txt" -s 0.60 -z 10000 -d 5455350 -r "/usr/local/bin/shuf" -i "n_ps-bible-lg-0.60-10k_spam" -o "./results/n_ps-bible-lg-0.60-10k_results_spam.json" -n 5 --benchmarkRuntime --spam
-
 java -jar -Xmx120G ProseccoBenchmark.jar -f "/home/ez/data/sequence/bms-webview.txt" -s 0.01 -z 10000 -d 17880001 -r "/usr/local/bin/shuf" -i "n_ps-bms-webview-lg-0.01-10k_spam" -o "./results/n_ps-bms-webview-lg-0.01-10k_results_spam.json" -n 5 --benchmarkRuntime --spam
 java -jar -Xmx120G ProseccoBenchmark.jar -f "/home/ez/data/sequence/bms-webview.txt" -s 0.025 -z 10000 -d 17880001 -r "/usr/local/bin/shuf" -i "n_ps-bms-webview-lg-0.025-10k_spam" -o "./results/n_ps-bms-webview-lg-0.025-10k_results_spam.json" -n 5 --benchmarkRuntime --spam
 java -jar -Xmx120G ProseccoBenchmark.jar -f "/home/ez/data/sequence/bms-webview.txt" -s 0.05 -z 10000 -d 17880001 -r "/usr/local/bin/shuf" -i "n_ps-bms-webview-lg-0.05-10k_spam" -o "./results/n_ps-bms-webview-lg-0.05-10k_results_spam.json" -n 5 --benchmarkRuntime --spam
@@ -22,5 +18,7 @@ java -jar -Xmx120G ProseccoBenchmark.jar -f "/home/ez/data/sequence/msnbc.txt" -
 java -jar -Xmx120G ProseccoBenchmark.jar -f "/home/ez/data/sequence/msnbc.txt" -s 0.30 -z 10000 -d 3179000 -r "/usr/local/bin/shuf" -i "n_ps-msnbc-lg-0.30-10k_spam" -o "./results/n_ps-msnbc-lg-0.30-10k_results_spam.json" -n 5 --benchmarkRuntime --spam
 java -jar -Xmx120G ProseccoBenchmark.jar -f "/home/ez/data/sequence/msnbc.txt" -s 0.40 -z 10000 -d 3179000 -r "/usr/local/bin/shuf" -i "n_ps-msnbc-lg-0.40-10k_spam" -o "./results/n_ps-msnbc-lg-0.40-10k_results_spam.json" -n 5 --benchmarkRuntime --spam
 
-
+java -jar -Xmx120G ProseccoBenchmark.jar -f "/home/ez/data/sequence/bible.txt" -s 0.40 -z 10000 -d 5455350 -r "/usr/local/bin/shuf" -i "n_ps-bible-lg-0.40-10k_spam" -o "./results/n_ps-bible-lg-0.40-10k_results_spam.json" -n 5 --benchmarkRuntime --spam
+java -jar -Xmx120G ProseccoBenchmark.jar -f "/home/ez/data/sequence/bible.txt" -s 0.50 -z 10000 -d 5455350 -r "/usr/local/bin/shuf" -i "n_ps-bible-lg-0.50-10k_spam" -o "./results/n_ps-bible-lg-0.50-10k_results_spam.json" -n 5 --benchmarkRuntime --spam
+java -jar -Xmx120G ProseccoBenchmark.jar -f "/home/ez/data/sequence/bible.txt" -s 0.60 -z 10000 -d 5455350 -r "/usr/local/bin/shuf" -i "n_ps-bible-lg-0.60-10k_spam" -o "./results/n_ps-bible-lg-0.60-10k_results_spam.json" -n 5 --benchmarkRuntime --spam
 
